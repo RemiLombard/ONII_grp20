cd968bf102b65b60b8f4995c389f112beb87573d
Rémi-Lombard <remi.lombard@edu.univ-fcomte.fr>
2024-05-22 18:25:02 +0200

    chore: Remove unused files and styles

---
+++ ONII/src/components/HeaderPage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+    import IconLogo from './icons/IconLogo.vue';
+
+</script>
+
+<template>
+    <header class="header">
+        <div class="">
+            <RouterLink to="/">
+                <IconLogo/>
+            </RouterLink>
+        </div>
+    </header>
+</template>
