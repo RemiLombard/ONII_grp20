3f0b4fe0c06fa77c485e9f2f8a724959b51b5efa
Rémi-Lombard <remi.lombard@edu.univ-fcomte.fr>
2024-06-12 16:25:17 +0200

    chgmt url

--- src/components/CardCom.vue
+++ src/components/CardCom.vue
@@ -35,7 +35,7 @@ const showModal = ref(false)
 
 const userAvatar = computed(() => {
   return props.avatar
-    ? `https://onii.remilombard.fr/api/files/_pb_users_auth_/${props.userId}/${props.avatar}`
+    ? `https://onii.remilombard.fr:443/api/files/_pb_users_auth_/${props.userId}/${props.avatar}`
     : defaultAvatar
 })
 
--- src/pages/reseau/DreamPost.vue
+++ src/pages/reseau/DreamPost.vue
@@ -116,7 +116,7 @@ const formattedDate = computed(() => {
 
 const userAvatar = computed(() => {
   return userProfile.value && userProfile.value.avatar
-    ? `https://onii.remilombard.fr/api/files/${userProfile.value.collectionId}/${userProfile.value.id}/${userProfile.value.avatar}`
+    ? `https://onii.remilombard.fr:443/api/files/${userProfile.value.collectionId}/${userProfile.value.id}/${userProfile.value.avatar}`
     : defaultAvatar
 })
 
